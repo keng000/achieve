d567f689e7206142cffeecc9c2896f81d507107d
Kengo Miyakawa <s_flashback@yahoo.co.jp>
2016-08-28 07:30:03 +0000

    add view_the_profile_image, next DIVE14_task

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -45,4 +46,12 @@ class User < ActiveRecord::Base
     SecureRandom.uuid
   end
   
+  def update_with_password(params, *options)
+    if provider.blank?
+      super
+    else
+      params.delete :current_password
+      update_without_password(params, *options)
+    end
+  end
 end
