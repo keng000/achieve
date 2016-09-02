bd26bc29c5642e557e9ee6e30a9c40a2911ce008
宮川健吾 <kengo@kenn.local>
2016-09-02 14:32:39 +0900

    message_func, yet push_call_func

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   resources :tasks
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
@@ -34,6 +35,10 @@ Rails.application.routes.draw do
         get 'inbox'
       end
     end
+  end
+
+  resources :conversations do
+    resources :messages
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
